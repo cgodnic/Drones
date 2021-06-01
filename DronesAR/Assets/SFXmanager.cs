d56a77378d5102d5526cddaef7ece74d7fa49dc3
GoldenJocker <72922387+GoldenJocker@users.noreply.github.com>
2021-06-02 00:21:04 +0200

    Background music

---
+++ DronesAR/Assets/SFXmanager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXmanager : MonoBehaviour
+{
+
+    public AudioSource Audio;
+    public AudioClip Click;
+
+    public static SfxManager sfxInstance;
+
+    private void Awake(){
+        if(BgInstance != null )
+    }
+}
